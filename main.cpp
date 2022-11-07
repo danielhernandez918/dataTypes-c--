4fac0d5fb40c2a48e8024b4cf0addd9f9646ca10
Daniel Hernandez <danielhernandez918@gmail.com>
2022-11-07 15:04:44 -0800

    first commit

---
+++ main.cpp
@@ -0,0 +1,14 @@
+#include <iostream>
+
+using namespace std;
+int main () 
+{
+    char grade ='A';
+    string phrase = "This is a string";
+    int age = 29;
+    double gpa = 3.9;
+    bool isMale = true;
+    cout << grade << endl;
+
+    return 0;
+}
